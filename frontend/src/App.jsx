30335799074cfe8ce968e3e3fb1929badeb53460
Roger Paiva <rogerpaiva1801@Gmail.com>
2026-04-14 14:31:54 -0300

    feat: Creating a banner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <>
       <Header />
+      <Banner />
       <Footer />
-    </div>
+    </>
   );
 }
 
