2f11f8d5b9c8f7cbb5a8706fead69179bbdfdaaa
Pedro <pedrot.ytb@gmail.com>
2026-04-15 20:17:52 -0300

    refactor(login): move page styles next to Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ import {
 import { FaApple } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import ProductLayout from "../components/ProductLayout";
-import "../styles/login.css";
+import "./Login.css";
 
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const initialValues = {
